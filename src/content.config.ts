e250ad467ef46bfe6e1ad0ce4b0185b1871906ac
7Linxx <lcalderonmo@unbosque.edu.co>
2026-02-22 12:29:54 -0500

    Change publishDate type to coerced date

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,10 @@
+// 1. Import utilities from `astro:content`
 import { defineCollection, z } from 'astro:content'
+
+// 2. Import loader(s)
 import { glob } from 'astro/loaders'
 
+// 3. Define your collection(s)
 const projects = defineCollection({
   loader: glob({ pattern: '**/*.mdx', base: './src/content/projects' }),
   schema: z.object({
@@ -18,8 +22,9 @@ const posts = defineCollection({
     author: z.string(),
     description: z.string(),
     tags: z.array(z.string()).default([]),
-    publishDate: z.string(),
+    publishDate: z.coerce.date(),
   }),
 })
 
+// 4. Export a single `collections` object to register you collection(s)
 export const collections = { projects, posts }
